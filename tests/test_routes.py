29daef9b6e20859733f0b4b498fc43980a7dcaee
dan h <danhirlinger@gmail.com>
2025-02-17 13:11:56 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,11 +22,13 @@ BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
+
 
     @classmethod
     def setUpClass(cls):
@@ -37,6 +39,7 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
+
 
     @classmethod
     def tearDownClass(cls):
@@ -52,6 +55,7 @@ class TestAccountService(TestCase):
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
+
 
     ######################################################################
     #  H E L P E R   M E T H O D S
